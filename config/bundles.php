02461e5b65a436e785b7cfcd18eb73f362ae1a7c
Georgiy Sukhomlin <g.sukhomlin@modesco.ru>
2026-02-25 21:20:04 +0200

    nutgram/symfony-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    SergiX44\NutgramBundle\NutgramBundle::class => ['all' => true],
 ];
