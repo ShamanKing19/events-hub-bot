46445141ca090484df4fdd5637400aecdd186e0d
Georgiy Sukhomlin <g.sukhomlin@modesco.ru>
2026-03-13 21:16:58 +0200

    Revert "symfony/security-bundle"
    This reverts commit a63a9ee62109dbc61bcb7c93d27612a40655139e.

--- config/bundles.php
+++ config/bundles.php
@@ -7,5 +7,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     SergiX44\NutgramBundle\NutgramBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
